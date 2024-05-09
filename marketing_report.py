df1c996612ef779151104804bbf732623c5aea7f
OneMin <grus272@gmail.com>
2024-05-09 23:52:00 +0900

    connect google sheet and exception handling

--- marketing_report.py
+++ marketing_report.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont, QFontDatabase
-from PyQt5.QtCore import Qt, QPoint, QThread, pyqtSignal
+from PyQt5.QtCore import Qt, QPoint, QThread, pyqtSignal, QObject
 import pymysql
 from functools import partial
 import time
 from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 class CustomButton(QPushButton):
     def __init__(self, text, parent=None):
@@ -55,45 +57,169 @@ class MySQLConnector:
                 password=self.password,
                 database=self.database
             )
-            print("MySQL 데이터베이스에 연결되었습니다.")
+            # print("MySQL 데이터베이스에 연결되었습니다.")
         except Exception as e:
             print("MySQL 데이터베이스 연결 오류:", e)
 
     def disconnect(self):
         if self.connection:
-            self.connection.close()
-            print("MySQL 데이터베이스 연결이 닫혔습니다.")
+            if self.connection.open:
+                self.connection.close()
+                # print("MySQL 데이터베이스 연결이 닫혔습니다.")
 
     def execute_query(self, query):
         try:
             if self.connection:
                 with self.connection.cursor() as cursor:
                     cursor.execute(query)
+                    result = cursor.fetchall()
                     self.connection.commit()
-                    print("쿼리가 성공적으로 실행되었습니다.")
+                    return result
         except Exception as e:
             self.connection.rollback()
             print("쿼리 실행 오류:", e)
 
-class Button1Clicked(CustomButton):
-    def __init__(self, main_window, autocompletion_input_field, id_text_field, name_text_field, longtext_text_field, link_text_field, parent=None):
-        super(Button1Clicked, self).__init__("Insert Data", parent)
+class GoogleSpreadsheetConnector:
+    def __init__(self, credentials_file, spreadsheet_name):
+        self.credentials_file = credentials_file
+        self.spreadsheet_name = spreadsheet_name
+
+    def insert_data(self, data, table_name):
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(self.credentials_file, scope)
+        gc = gspread.authorize(credentials)
+        spreadsheet = gc.open(self.spreadsheet_name)
+        sheet_name = table_name
+        try:
+            self.sheet = spreadsheet.worksheet(sheet_name)
+        except gspread.exceptions.WorksheetNotFound:
+            # sheet가 존재하지 않을때 sheet 생성
+            self.sheet = spreadsheet.add_worksheet(title=sheet_name, rows=1000, cols=7)
+            headers = ["id", "name", "text", "link", "datetime"]
+            self.sheet.insert_row(headers, index=1)
+
+            # 1행 고정
+            self.sheet.freeze(rows=1)
+            # a-g 열 그룹화
+            self.sheet.add_protected_range('A1:G1')
+
+            # 모든 행 중앙 정렬
+            last_row = 1000  # 마지막 행은 1000으로 설정
+            format_request = {
+                "repeatCell": {
+                    "range": {
+                        "sheetId": self.sheet.id,
+                        "startRowIndex": 0,
+                        "endRowIndex": last_row,
+                        "startColumnIndex": 0,
+                        "endColumnIndex": 7
+                    },
+                    "cell": {
+                        "horizontalAlignment": "CENTER"
+                    },
+                    "fields": "userEnteredFormat.horizontalAlignment"
+                }
+            }
+            self.sheet.batch_update({'requests': [format_request]})
+        #데이터 입력
+        self.sheet.append_row(data)
+
+
+class CheckTextFieldExistence(QObject):
+    table_existence_checked = pyqtSignal(bool)
+
+    def __init__(self, mysql_connector, table_name, id_text_field, name_text_field):
+        super().__init__()
+        self.mysql_connector = mysql_connector
+        self.table_name = table_name
+        self.id_text_field = id_text_field
+        self.name_text_field = name_text_field
+
+    def check_fields_existence(self):
+        # Check if the table exists
+        self.mysql_connector.connect()
+        query = f"SHOW TABLES LIKE '{self.table_name}'"
+        result = self.mysql_connector.execute_query(query)
+        table_exists = bool(result)
+        if not table_exists:
+            self.table_existence_checked.emit(False)
+            self.mysql_connector.disconnect()
+            return
+
+        # Check if the id and name exist in their respective columns
+        id_value = self.id_text_field.text()
+        name_value = self.name_text_field.text()
+
+        # Check if id_value exists in the 'id' column of '분류' table
+        id_query = f"SELECT COUNT(*) FROM 분류 WHERE name = '{id_value}'"
+        id_exists = self.mysql_connector.execute_query(id_query)[0][0] > 0
+
+        # Check if name_value exists in the 'name' column of '작업자' table
+        name_query = f"SELECT COUNT(*) FROM 작업자 WHERE name = '{name_value}'"
+        name_exists = self.mysql_connector.execute_query(name_query)[0][0] > 0
+
+        self.table_existence_checked.emit(id_exists and name_exists)
+        self.mysql_connector.disconnect()
+
+
+class Input_Button_Clicked(CustomButton):
+    def __init__(self, main_window, autocompletion_input_field, id_text_field, name_text_field, longtext_text_field,
+                 link_text_field, parent=None):
+        super(Input_Button_Clicked, self).__init__("Insert Data", parent)
         self.main_window = main_window
         self.autocompletion_input_field = autocompletion_input_field
         self.id_text_field = id_text_field
         self.name_text_field = name_text_field
         self.longtext_text_field = longtext_text_field
         self.link_text_field = link_text_field
-        self.clicked.connect(self.insert_data)
+        self.clicked.connect(self.check_table_existence)
 
-    def insert_data(self):
-        print("1")
-        current_frame_index = self.main_window.stacked_widget.currentIndex()
-        current_frame = self.main_window.frames[current_frame_index]
+    def check_table_existence(self):
+        table_name = self.autocompletion_input_field.text()
+        checker = CheckTextFieldExistence(self.main_window.mysql_connector, table_name,
+                                          self.id_text_field, self.name_text_field)
+        checker.table_existence_checked.connect(self.handle_table_existence_checked)  # 수정된 부분
+        checker.check_fields_existence()  # 수정된 부분
 
+    def handle_table_existence_checked(self, table_exists):
+        if not table_exists:
+            QMessageBox.warning(self.main_window, "Table Not Found", "Table does not exist.")
+            return
+
+        # 데이터 삽입 메서드 호출
+        checker = CheckTextFieldExistence(self.main_window.mysql_connector, self.autocompletion_input_field.text(),
+                                          self.id_text_field, self.name_text_field)
+        checker.table_existence_checked.connect(self.handle_fields_existence_checked)  # 수정된 부분
+        checker.check_fields_existence()  # 수정된 부분
+
+    def handle_fields_existence_checked(self, fields_existence):
+        if not fields_existence:
+            QMessageBox.warning(self.main_window, "Field Not Found", "Field does not exist.")
+            return
+
+        # 데이터 삽입 메서드 호출
+        self.Change_classification_text()
+
+    def Change_classification_text(self):
         # 데이터베이스에 연결
         self.main_window.mysql_connector.connect()
 
+        cursor = self.main_window.mysql_connector
+        id_query = "SELECT DISTINCT id FROM `분류`"
+        name_query = "SELECT DISTINCT name FROM `분류`"
+        id_values = cursor.execute_query(id_query)
+        id_values = [str(id[0]) for id in id_values]
+        name_values = cursor.execute_query(name_query)
+        name_values = [name[0] for name in name_values]
+        self.id_text = id_values[name_values.index(self.id_text_field.text())]
+
+        # 데이터베이스 연결 닫기
+        self.main_window.mysql_connector.disconnect()
+        self.insert_data()
+
+    def insert_data(self):
+        # 데이터베이스에 연결
+        self.main_window.mysql_connector.connect()
         # 데이터 삽입
         table_name = self.autocompletion_input_field.text()
         id_text = self.id_text_field.text()
@@ -101,15 +227,18 @@ class Button1Clicked(CustomButton):
         text_text = self.longtext_text_field.text()
         link_text = self.link_text_field.text()
         current_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        query = f"INSERT INTO {table_name} (id, `index`, name, text, link, datetime) VALUES ('{id_text}', 1, '{name_text}', '{text_text}', '{link_text}', '{current_datetime}')"
+        query = f"INSERT INTO {table_name} (id, name, text, link, datetime) VALUES ('{self.id_text}', '{name_text}', '{text_text}', '{link_text}', '{current_datetime}')"
         self.main_window.mysql_connector.execute_query(query)
+
+        # Google Sheet에 데이터 삽입
+        data = [id_text, name_text, text_text, link_text, current_datetime]
+        self.main_window.google_sheet_connector.insert_data(data, table_name)
 
         # 데이터베이스 연결 닫기
         self.main_window.mysql_connector.disconnect()
 
         # 성공 메시지 표시
         QMessageBox.information(self.main_window, "Success", "데이터가 성공적으로 삽입되었습니다.")
-
 
 class MySQLInputDataThread(QThread):
     def __init__(self, button_clicked_object):
@@ -143,7 +272,22 @@ class AutocompletionThread(QThread):
 
                 # Extract table names
                 table_names = [name[0] for name in table_names]
-                self.result.emit(table_names)
+
+                # Query to get distinct values of id and name from the table
+                cursor.execute("SELECT DISTINCT name FROM 분류")
+                id_values = cursor.fetchall()
+
+                cursor.execute("SELECT DISTINCT name FROM 작업자")
+                name_values = cursor.fetchall()
+
+                # Extract values
+                id_values = [id[0] for id in id_values]
+                name_values = [name[0] for name in name_values]
+
+                # Combine table_names and id and name values
+                autocompletion_data = [table_names] + [id_values] + [name_values]
+
+                self.result.emit(autocompletion_data)
 
                 # Close Cursor and Connection
                 cursor.close()
@@ -221,7 +365,7 @@ class MainWindow(QMainWindow):
                             border-radius: 8px;
                             """)
 
-        self.setWindowTitle("프레임 전환 UI")
+        self.setWindowTitle("Example Program")
 
         main_layout = QVBoxLayout()
 
@@ -260,7 +404,6 @@ class MainWindow(QMainWindow):
 
         # 프레임 생성 및 스택 위젯에 추가
         self.frames = []
-        self.colums = ["종류", "작업자", "내용", "링크"]
         for i in range(4):
             frame = FrameWithBar(self)
             frame.setFixedWidth(400)  # 프레임의 가로 크기를 30px 늘림
@@ -334,7 +477,7 @@ class MainWindow(QMainWindow):
                     button_layout.addWidget(button)
             else:  # 나머지 프레임에는 버튼 1개 추가
                 if i == 0:
-                    button = Button1Clicked(self, self.autocompletion_input_field, self.id_text, self.name_text,
+                    button = Input_Button_Clicked(self, self.autocompletion_input_field, self.id_text, self.name_text,
                                             self.longtext_text, self.link_text)
                     button.setText("Input Data")
                     button_layout.addWidget(button)
@@ -351,6 +494,13 @@ class MainWindow(QMainWindow):
 
         self.setLayout(main_layout)
 
+        # Google Sheet 연결 정보
+        credentials_file = 'sturdy-coast-318906-0f9b364f15a4.json'  # 다운로드한 JSON 키 파일 경로
+        spreadsheet_name = 'Example_Spread_Sheet'  # Google 스프레드시트 이름
+
+        # Google Sheet Connector 인스턴스 생성
+        self.google_sheet_connector = GoogleSpreadsheetConnector(credentials_file, spreadsheet_name)
+
         # MySQL 연결 정보
         host = "127.0.0.1"
         user = "Program_User"
@@ -366,14 +516,15 @@ class MainWindow(QMainWindow):
         self.autocompletion_thread.start()
 
     def mysql_input_data(self):
-        mysql_input_data_thread = MySQLInputDataThread(Button1Clicked(self, self.autocompletion_input_field, self.id_text, self.name_text, self.longtext_text, self.link_text))
+        mysql_input_data_thread = MySQLInputDataThread(Input_Button_Clicked(self, self.autocompletion_input_field, self.id_text, self.name_text, self.longtext_text, self.link_text))
         mysql_input_data_thread.start()
 
-    def update_completer(self, table_names):
-        completer = QCompleter(table_names, self)
-        for frame in self.frames:
-            autocompletion_input_field = frame.findChild(QLineEdit)
-            autocompletion_input_field.setCompleter(completer)
+    def update_completer(self, autocompletion_data):
+        input_fields = self.frames[0].findChildren(QLineEdit)
+        for index, data in enumerate(autocompletion_data):
+            completer = QCompleter(data, self)
+            input_field = input_fields[index]
+            input_field.setCompleter(completer)
 
     def switch_to_frame(self, index):
         if str(index).isdigit():
